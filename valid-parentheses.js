035a6694e9157b57ce9d878ba553019d03ecffa5
Ruben Langer <rubenjohanneslanger@gmail.com>
2022-04-01 12:14:53 +0200

    better solution

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,34 +1,29 @@
-// Input: String
-// Questions:
-// 1. Empty string? -> no
-// 2. Only valid characters (paretheses only)? -> yes
-
-// Output: Boolean
-// Questions:
-// 1. How many solutions? => always exactly one.
-
-// Algorithm:
-// 1. Determine sub-unit => each index contains one parentheses
-// 2. Check whether sub-unit is valid according to rules
-// 3. If all sub-units are valid, return true; else, return false
-
-var isValid = function(str) {
-
-    // Counter for thre different types of opening and closing brackets
-    let counterRoundedOpening = 0;
-    let counterRoundedClosing = 0;
-    let counterCurlyOpening = 0;
-    let counterCurlyClosing = 0;
-    let counterSquareOpening = 0;
-    let counterSquareClosing = 0;
-
-    // Memory for brackets interated over before current bracket
-    let previousBrackets = {};
-
-    // Looping through input string
-    for (let i = 0; i < str.length; i++) {
-        
-        
-    }
+var validBracketComplements = {
+    ")": "(",
+    "]": "[",
+    "}": "{"
 };
-
+var isValid = function (str) {
+    var openingBrackets = [];
+    for (var i = 0; i < str.length; i++) {
+        if (isClosingBracket(str[i])) {
+            // If curr elem is closing bracket, check whether previous opening bracket is valid complement
+            console.log(openingBrackets);
+            var complement = openingBrackets.pop();
+            console.log(complement);
+            if (validBracketComplements[str[i]] !== complement) {
+                console.log(i);
+                return false;
+            }
+        }
+        else {
+            console.log("got here!");
+            openingBrackets.push(str[i]);
+        }
+    }
+    return true;
+};
+var isClosingBracket = function (char) {
+    return validBracketComplements.hasOwnProperty(char);
+};
+console.log(isValid("([])"));
